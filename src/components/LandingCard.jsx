f88ad7fc6171b3e8aeaac203fa27ec5b67d51ce2
vcabolfazl <104507434+vcabolfazl@users.noreply.github.com>
2023-09-03 16:02:32 +0330

    feat : -> Development of Section 2

---
+++ src/components/LandingCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LandingCard({ icon, title, desc }) {
+ return (
+  <div className="bg-zinc-700/20 p-3 md:p-7 rounded-md">
+   <svg className="w-14 h-14">
+    <use href={`#${icon}`}></use>
+   </svg>
+   <h4 className='my-4 font-SatoshiBold text-lg'>{title}</h4>
+   <p className='text-gray'>{desc}</p>
+  </div>
+ )
+}
