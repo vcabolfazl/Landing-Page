079611d16be287a1eb01df193bfb919a7ebc374e
vcabolfazl <104507434+vcabolfazl@users.noreply.github.com>
2023-09-06 10:11:06 +0330

    chore : -> Fix navbar at top of the screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,15 +4,15 @@ import SVG from "@/components/SVG"
 import LandingCard from '@/components/LandingCard'
 import Slider from '@/components/Slider'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 export default function page() {
   const [showMenu, setShowMenu] = useState(false)
+
   return (
     <>
       <SVG />
 
       <div className="container">
-        <section className='my-16'>
+        <section className='my-32'>
 
           <div className="w-full absolute left-0 z-[-1] flex justify-between">
             <svg className='inline-block z-[-1]'
@@ -84,7 +84,7 @@ export default function page() {
               </defs>
             </svg>
           </div>
-          <div className="w-full mt-10 text-center" data-aos="fade-up">
+          <div className="w-full mt-10 text-center mb-16" data-aos="fade-up">
             <h1 className='font-SatoshiBold text-2xl md:text-3xl xl:text-5xl pb-4 title'>A CRM dashboard for engineering teams</h1>
             <p className='text-gray md:text-lg w-full xl:w-2/3 mx-auto mb-7'>Rorem ipsum dolor sit amet consectetur. Gravida convallis orci ultrices non. Ultricies tempor at ut cursus mi. Aliquam sed amet vitae orci ac penatibus consectetur.</p>
             <div className="gap-x-6 gap-y-2 my-7 flex justify-center flex-wrap">
@@ -98,43 +98,45 @@ export default function page() {
             </div>
             <img className='mx-auto' src="/assets/Images/HeroImage.png" alt="" />
           </div>
-          <nav className='sticky grid grid-cols-2 md:grid-cols-12 justify-between items-center mt-16'>
-            <div className="left md:col-span-2 z-30">
-              <svg className='h-10'>
-                <use href='#Logo'></use>
-              </svg>
-            </div>
+          <div className="w-full left-0 right-0 fixed z-50 bg-[#060B27] top-0 py-5 px-5">
+            <nav className={`container grid grid-cols-2 md:grid-cols-12 justify-between items-center top-0`}>
+              <div className="left md:col-span-2 z-30">
+                <svg className='h-10'>
+                  <use href='#Logo'></use>
+                </svg>
+              </div>
 
-            <div className="text-right block md:hidden transition-all z-30">
-              <svg className="w-9 h-9 inline-block" onClick={() => setShowMenu(status => !status)}>
-                <use href='#Menu'></use>
-              </svg>
-              <ul className={`absolute right-0 z-30 bg-zinc-700/20 rounded-xl w-full p-3 space-y-3
-            b-zinc-700 text-center top-14 text-gray ${showMenu ? "block opacity-100 translate-y-9" : "hidden opacity-0 "}`}>
-                <li className='text-white'>Home</li>
-                <li>About</li>
-                <li>Features</li>
-                <li>Pricing</li>
-                <li>Blog</li>
-              </ul>
-            </div>
-            <div className="center col-span-8  hidden text-center w-full md:flex items-center justify-center">
-              <ul className='flex gap-x-8 text-gray'>
-                <li className='text-white'>Home</li>
-                <li>About</li>
-                <li>Features</li>
-                <li>Pricing</li>
-                <li>Blog</li>
-              </ul>
-            </div>
-            <div className={`fixed h-full w-full bg-black/40 inset-0 ${showMenu ? "block z-10" : "hidden"}`} onClick={() => setShowMenu(status => !status)}></div>
-            <div className=" hidden md:block right text-right col-span-2">
-              <Button
-                title={"Get a Demo"}
-                bg={true}
-              />
-            </div>
-          </nav>
+              <div className="text-right block md:hidden transition-all z-30">
+                <svg className="w-9 h-9 inline-block" onClick={() => setShowMenu(status => !status)}>
+                  <use href='#Menu'></use>
+                </svg>
+                <ul className={`absolute right-0 z-30 bg-zinc-700/20 rounded-b-xl w-full p-3 space-y-3
+            b-zinc-700 text-center top-11 text-gray ${showMenu ? "block opacity-100 translate-y-9" : "hidden opacity-0 "}`}>
+                  <li className='text-white'>Home</li>
+                  <li>About</li>
+                  <li>Features</li>
+                  <li>Pricing</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
+              <div className="center col-span-8  hidden text-center w-full md:flex items-center justify-center">
+                <ul className='flex gap-x-8 text-gray'>
+                  <li className='text-white'>Home</li>
+                  <li>About</li>
+                  <li>Features</li>
+                  <li>Pricing</li>
+                  <li>Blog</li>
+                </ul>
+              </div>
+              <div className={`fixed h-full w-full bg-black/40 inset-0 ${showMenu ? "block z-10" : "hidden"}`} onClick={() => setShowMenu(status => !status)}></div>
+              <div className=" hidden md:block right text-right col-span-2">
+                <Button
+                  title={"Get a Demo"}
+                  bg={true}
+                />
+              </div>
+            </nav>
+          </div>
 
         </section>
 
